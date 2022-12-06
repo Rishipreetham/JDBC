3f857c16c168cb10433701c580d9229c0dfc43d7
Rishipreetham lingampally <61833673+Rishipreetham@users.noreply.github.com>
2022-12-06 13:04:07 +0530

    Add files via upload

---
+++ JDBC_Assignment1/src/com/ineuron/main/TestApp.java
@@ -0,0 +1,41 @@
+package com.ineuron.main;
+
+import java.util.Scanner;
+
+import com.ineuron.operations.CreateOperation;
+import com.ineuron.operations.DeleteOperation;
+import com.ineuron.operations.ReadOperation;
+import com.ineuron.operations.UpdateOperation;
+
+public class TestApp {
+
+	public void input()
+	{
+		System.out.println();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("please enter as  per the operation listed below on student table\r\n"
+				+ "1. Create 2. Read 3. Update 4. Delete");
+		System.out.println();
+		System.out.println("enter input");
+		int input=sc.nextInt();
+		switch(input)
+		{
+		case 1:CreateOperation.create();
+				break;
+		case 2:ReadOperation.read();
+				break;
+		case 3:UpdateOperation.update();
+				break;
+		case 4:DeleteOperation.delete();
+				break;
+		default:System.out.println("please enter valid input");	
+			
+		}
+	}
+	public static void main(String[] args) {
+
+		TestApp obj = new TestApp();
+		obj.input();
+	}
+
+}
---
+++ JDBC_Assignment1/src/com/ineuron/operations/CreateOperation.java
@@ -0,0 +1,70 @@
+
+package com.ineuron.operations;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+import com.ineuron.util.JdbcConnection;
+
+public class CreateOperation {
+
+	public static void create() {
+
+		//Resources
+		Connection connection=null;
+		Statement stmt=null;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		try
+		{
+			connection=JdbcConnection.getConnection();
+			if(connection!=null)
+			{
+				stmt=connection.createStatement();
+			}
+			
+			System.out.println("enter id");
+			int id=sc.nextInt();
+			
+			System.out.println("enter name");
+			String name=sc.next();
+			
+			System.out.println("enter age");
+			int age=sc.nextInt();
+			
+			System.out.println("enter address");
+			String address=sc.next();
+			
+			String insertquery=String.format("insert into student values(%d,'%s',%d,'%s')",id,name,age,address);
+			if(stmt!=null)
+			{
+				int noofrows=stmt.executeUpdate(insertquery);
+				System.out.println("No of rows inserted are ::"+noofrows);
+			}
+			
+			
+		}
+		catch(SQLException se)
+		{
+			se.printStackTrace();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try {
+				JdbcConnection.removeConnection(null, stmt, connection);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			if(sc!=null)
+				sc.close();
+		}
+	}
+
+}
